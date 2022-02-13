0f88e2140a2c54f7c30bdac576ea2cef6761f353
Lê Văn Tuân <51940082+letuan02@users.noreply.github.com>
2022-02-13 20:51:26 +0700

    update add wishlist

--- src/pages/user/searchProduct.js
+++ src/pages/user/searchProduct.js
@@ -1,8 +1,12 @@
-import { search } from "../../api/product";
+import toastr from "toastr";
+import { get, search, update } from "../../api/product";
 import Footer from "../../components/user/footer";
 import Header from "../../components/user/header";
 import Sidebar from "../../components/user/products/sidebar";
-import { formatCurrency } from "../../utils";
+import { formatCurrency, getUser, reRender } from "../../utils";
+import WishList from "../../components/user/wishlist";
+import WishListLabel from "../../components/user/wishlistLabel";
+import { add, checkHeart } from "../../api/favoritesProduct";
 
 const SearchProductPage = {
     async render(keyword) {
@@ -81,7 +85,7 @@ const SearchProductPage = {
                                     <div class="relative bg-[#f7f7f7] overflow-hidden">
                                         <a href="/#/product/${item.id}" style="background-image: url(${item.image})" class="bg-cover pt-[100%] bg-center block"></a>
                                         <button class="absolute w-full bottom-0 h-9 bg-[#D9A953] text-center text-gray-50 opacity-95 uppercase font-semibold text-sm transition ease-linear duration-300 hover:opacity-100 hover:text-white translate-y-full group-hover:translate-y-0">Xem nhanh</button>
-                                        <button class="absolute top-3 right-3 w-8 h-8 rounded-full border-2 text-[#c0c0c0] text-lg border-[#c0c0c0] transition duration-300 hover:text-white hover:bg-red-700 hover:border-red-700 opacity-0 group-hover:opacity-100">
+                                        <button data-id="${item.id}" class="btn-heart absolute top-3 right-3 w-8 h-8 rounded-full border-2 text-[#c0c0c0] text-lg border-[#c0c0c0] transition duration-300 hover:text-white hover:bg-red-700 hover:border-red-700 opacity-0 group-hover:opacity-100">
                                             <i class="fas fa-heart"></i>
                                         </button>
                                     </div>
@@ -170,6 +174,44 @@ const SearchProductPage = {
     afterRender() {
         Header.afterRender();
         Footer.afterRender();
+
+        // yêu thích sp
+        const btnsHeart = document.querySelectorAll(".btn-heart");
+        btnsHeart.forEach((btn) => {
+            const { id } = btn.dataset;
+
+            btn.addEventListener("click", async () => {
+                const userLogged = getUser();
+
+                if (!userLogged) {
+                    toastr.info("Vui lòng đăng nhập để yêu thích sản phẩm");
+                } else {
+                    const { data: heartList } = await checkHeart(userLogged.id, id);
+
+                    if (heartList.length) {
+                        toastr.info("Sản phẩm đã tồn tại trong danh sách yêu thích");
+                    } else {
+                        const { data: productInfo } = await get(id);
+                        productInfo.favorites += 1;
+
+                        // cập nhật số lượt yêu thích
+                        update(id, productInfo);
+
+                        // lưu thông tin
+                        const date = new Date();
+                        add({
+                            userId: userLogged.id,
+                            productId: +id,
+                            createdAt: date.toISOString(),
+                        })
+                            .then(() => toastr.success("Đã thêm vào danh sách yêu thích"))
+                            .then(() => reRender(WishListLabel, ".header-icon-heart"))
+                            .then(() => reRender(WishList, "#wishlist"))
+                            .then(() => document.querySelector("#wishlist").classList.add("active"));
+                    }
+                }
+            });
+        });
     },
 };
 
