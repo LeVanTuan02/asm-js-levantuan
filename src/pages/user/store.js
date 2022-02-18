745984c09abdfa31d65be1143be8a0f6d2989379
Lê Văn Tuân <51940082+letuan02@users.noreply.github.com>
2022-02-18 16:49:08 +0700

    style: update css store page

--- src/pages/user/store.js
+++ src/pages/user/store.js
@@ -18,7 +18,7 @@ const StorePage = {
                     <div class="col-span-12 md:col-span-5">
                         <p>Vui lòng chọn khu vực bạn muốn tìm kiếm, chúng tôi sẽ hiển thị danh sách các cửa hàng phù hợp nhất</p>
 
-                        <form action="" class="flex mt-3">
+                        <form action="" class="flex mt-3" id="form-search-store">
                             <input type="text" placeholder="Nhập tên chi nhánh" id="store__form-search-control" class="h-12 rounded-l-full px-5 border flex-1 outline-none">
                             <button type="button" class="bg-[#D9A953] px-5 text-white font-extrabold text-2xl rounded-r-full">
                                 <i class="fas fa-chevron-down"></i>
@@ -27,10 +27,10 @@ const StorePage = {
 
                         <ul class="mt-3 border-r h-[400px] overflow-y-scroll" id="store__list">
                             ${storeList.map((store, index) => /* html */`
-                                <li data-id="${store.id}" class="store__list-item ${index === 0 ? "active" : ""} flex py-4 transition ease-linear duration-300 hover:bg-gray-50 cursor-pointer px-3">
+                                <li data-id="${store.id}" class="store__list-item ${index === 0 ? "active" : ""} flex py-4 transition ease-linear duration-300 hover:bg-gray-50 cursor-pointer px-3 items-center">
                                     <img class="w-36 h-24 object-cover" src="${store.image}" alt="">
 
-                                    <div class="ml-3 leading-6">
+                                    <div class="ml-3 leading-6 flex-1">
                                         <h2 class="font-semibold text-[#D9A953] text-lg">${store.name}</h2>
                                         <p>${store.address}</p>
                                         <p>${store.timeStart} - ${store.timeEnd}</p>
@@ -79,10 +79,10 @@ const StorePage = {
             const keyword = e.target.value;
             const { data } = await search(keyword);
             const html = data.map((store, index) => `
-                <li data-id="${store.id}" class="store__list-item ${index === 0 ? "active" : ""} flex py-4 transition ease-linear duration-300 hover:bg-gray-50 cursor-pointer px-3">
+                <li data-id="${store.id}" class="store__list-item ${index === 0 ? "active" : ""} flex py-4 transition ease-linear duration-300 hover:bg-gray-50 cursor-pointer px-3 items-center">
                     <img class="w-36 h-24 object-cover" src="${store.image}" alt="">
 
-                    <div class="ml-3 leading-6">
+                    <div class="ml-3 leading-6 flex-1">
                         <h2 class="font-semibold text-[#D9A953] text-lg">${store.name}</h2>
                         <p>${store.address}</p>
                         <p>${store.timeStart} - ${store.timeEnd}</p>
@@ -95,6 +95,9 @@ const StorePage = {
 
             tabMap();
         });
+
+        const formSearch = document.querySelector("#form-search-store");
+        formSearch.addEventListener("submit", (e) => e.preventDefault());
     },
 
 };
