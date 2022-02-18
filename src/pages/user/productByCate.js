bf065d4d968eb4ce88e7d14e08671a34183ef3c3
Lê Văn Tuân <51940082+letuan02@users.noreply.github.com>
2022-02-18 15:42:12 +0700

    refactor: update limit page

--- src/pages/user/productByCate.js
+++ src/pages/user/productByCate.js
@@ -14,7 +14,7 @@ const ProductByCatePage = {
         const { data } = await getProductByCate(cateId);
 
         // phân trang
-        const limit = 3; // limit
+        const limit = 12; // limit
         const total = data.length; // tổng số sp
         const totalPage = Math.ceil(total / limit); // tổng số page
         let currentPage = pageNumber ?? 1; // lấy số trang hiện tại
