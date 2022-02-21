81c2e12cd75603791e1da19346bb7c7ae5f45836
Lê Văn Tuân <51940082+letuan02@users.noreply.github.com>
2022-02-21 22:22:25 +0700

    refactor: update api size

--- src/api/size.js
+++ src/api/size.js
@@ -3,7 +3,7 @@ import instance from "./config";
 const TABLE_NAME = "sizes";
 
 export const getAll = () => {
-    const url = `/${TABLE_NAME}/?_sort=id&_order=desc`;
+    const url = `/${TABLE_NAME}/?_sort=name&_order=desc`;
     return instance.get(url);
 };
 
